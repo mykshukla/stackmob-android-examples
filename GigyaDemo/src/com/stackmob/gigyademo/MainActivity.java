ceffb0df9f07f66d01ab812d2a8b21a8e1356cd9
Douglas Rapp <drapp@stackmob.com>
2013-02-14 16:16:20 -0800

    condense this into one class

--- GigyaDemo/src/com/stackmob/gigyademo/MainActivity.java
+++ GigyaDemo/src/com/stackmob/gigyademo/MainActivity.java
@@ -1,10 +1,15 @@
 package com.stackmob.gigyademo;
 
-import gigya.LoginUIListener;
-
+import com.gigya.socialize.GSKeyNotFoundException;
+import com.gigya.socialize.GSObject;
+import com.gigya.socialize.GSResponse;
 import com.gigya.socialize.android.GSAPI;	
+import com.gigya.socialize.android.event.GSLoginUIListener;
 import com.stackmob.android.sdk.common.StackMobAndroid;
 import com.stackmob.sdk.api.StackMob;
+import com.stackmob.sdk.api.StackMobOptions;
+import com.stackmob.sdk.callback.StackMobCallback;
+import com.stackmob.sdk.exception.StackMobException;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -12,8 +17,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements GSLoginUIListener {
 
+	private User user = new User();
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +30,36 @@ public class MainActivity extends Activity {
 		StackMob.getStackMob().getSession().getLogger().setLogging(true);
 		
 		GSAPI gigya = new GSAPI("YOUR_GIGYA_KEY", this);
-		gigya.showLoginUI(null, new LoginUIListener(this), null);
+		gigya.showLoginUI(null, this, null);
+    }
+    
+	@Override
+	public void onLogin(String provider, GSObject user, Object context) {
+		try {
+			String uid = user.getString("UID");
+			String timestamp = user.getString("signatureTimestamp");
+			String sig = user.getString("UIDSignature");
+			this.user.loginWithGigya(uid, timestamp, sig, StackMobOptions.none(), new StackMobCallback() {
+				
+				@Override
+				public void success(String arg0) {
+					MainActivity.this.gotLoggedInUser(MainActivity.this.user);
+				}
+				
+				@Override
+				public void failure(StackMobException arg0) {
+					arg0.printStackTrace();
+					
+				}
+			});
+		} catch (GSKeyNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+    public void gotLoggedInUser(User user) {
+    	System.out.println("Got user " + user.toJson());
     }
 
     @Override
@@ -31,10 +67,26 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-    
-    public void gotLoggedInUser(User user) {
-    	System.out.println("Got user " + user.toJson());
-    }
+
+	@Override
+	public void onClose(boolean arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onError(GSResponse arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onLoad(Object arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 
     
 }
